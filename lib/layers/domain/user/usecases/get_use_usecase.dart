33100b989b459d7507a232f8aa9e1842be7dd61b
Werton <mr.werton@gmail.com>
2023-01-27 05:31:30 -0300

    feat: created usecase for get user

---
+++ lib/layers/domain/user/usecases/get_use_usecase.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
+import 'package:flutter_clean_arch/common/error/failures.dart';
+import 'package:flutter_clean_arch/common/usecase/usecase.dart';
+import 'package:flutter_clean_arch/layers/domain/user/entities/user.dart';
+import 'package:flutter_clean_arch/layers/domain/user/repositories/user_repository.dart';
+
+class GetUserUseCase implements UseCase<User, GetUserParams> {
+  final UserRepository _userRepository;
+  GetUserUseCase({
+    required UserRepository userRepository,
+  }) : _userRepository = userRepository;
+
+  @override
+  Future<Either<Failure, User>> call(GetUserParams params) async {
+    final id = params.id;
+
+    return await _userRepository.get(id: id);
+  }
+}
+
+class GetUserParams {
+  final String id;
+  GetUserParams({
+    required this.id,
+  });
+}
